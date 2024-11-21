f669e02235f2cc076b4f0b0d686dc72486786bd1
azizmokhtar <mohamedazizelmokhtar27@gmail.com>
2024-11-21 17:54:19 +0100

    Bus class implementaion

--- backend/models/BusManagementSystem.py
+++ backend/models/BusManagementSystem.py
@@ -11,23 +11,55 @@ class Bus:
         self.electric_capacity = electric_capacity
         self.seat_capacity = seat_capacity
 
-    def AddBus():
-        # get the database and write to it
-        print("New bus added")
 
-    def DeleteBus():
-        # get the database and remove from it
-        print("Bus deleted")
+    #  delet the current station object to the database
+    def delete_from_database(self):
+        conn = self.db_manager.connect()
+        cur = conn.cursor()
+        # Delete the station record where the number matches
+        cur.execute("DELETE FROM buses WHERE number = ?", (self.number,))
+        conn.commit()
+        print(f"Bus '{self.number}' deleted from database.")
 
-    # fetch the bus by a dertain param
-    def FetchBus(param):
-        # get the database and..
-            # if licenseplate
-            # elif dateofpurchase
-            # elif status
-            # stateofcharge
-            # ..
-        print("Bus fetched:")
+
+    # Edit any station attribute dynamically
+    def EditBus(self, param, value):    
+        conn = self.db_manager.connect()
+        if hasattr(self, param):
+            setattr(self, param, value)
+        cur = conn.cursor()
+        cur.execute(
+            f"UPDATE bus SET {param} = ? WHERE number = ?",
+            (value, self.number)
+        )
+        conn.commit()
+        print(f"Bus '{self.number}' updated: {param} = {value}")
+
+
+    # save the current bus object to the database
+    def save_to_database(self):
+        conn = self.db_manager.connect()
+        cur = conn.cursor()
+        cur.execute(
+            "INSERT INTO buses (license_plate, date_of_purchase, status, state_of_charge, electric_capacity, seat_capacity) VALUES (?, ?, ?, ?, ?, ?)",
+            (self.number, self.status, self.state_of_charge, self.electric_capacity)
+        )
+        conn.commit()
+        print(f"bus '{self.number}' saved to database.")
+
+    # Fetch a bus by parameter
+    def fetch_station_through_params(self, param, value):
+        conn = self.db_manager.connect()
+        cur = conn.cursor()
+        query = f"SELECT * FROM buses WHERE {param} = ?"
+        cur.execute(query, (value,))
+        rows = cur.fetchall()
+        if rows:
+            print("Found:")
+            for row in rows:
+                print(row)
+        else:
+            print(f"No bus found with {param} = {value}.")
 
 
     
