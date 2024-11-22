221167a34892b25f3516cb79551d70e3c8ce0560
Achraf Hafsaoui <achraf.hafsaoui@gmail.com>
2024-11-22 19:40:28 +0100

    deleted duplicate folder

--- teamprojekt_backend/app/admin.py
+++
@@ -1,3 +0,0 @@
-from django.contrib import admin
-
-# Register your models (databases) that will be viewed by admin panel here.
--- teamprojekt_backend/app/views.py
+++
@@ -1,6 +0,0 @@
-from django.shortcuts import render, HttpResponse
-
-# Create your views here.
-
-def overview(requests):
-    return HttpResponse("Hello, world!")
