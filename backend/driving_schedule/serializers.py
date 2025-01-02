7544e080819b4f455d80b2c2a8a1a100cdf5893a
Aziz Abouda <azizabouda0103@gmail.com>
2025-01-02 23:34:43 +0100

    display of the driving schedules fixed

--- backend/driving_schedule/serializers.py
+++ backend/driving_schedule/serializers.py
@@ -1,4 +1,3 @@
-# driving_schedule/serializers.py
 from rest_framework import serializers
 from .models import DrivingSchedule, LocationType
 from busMonitoring.models import ElectricBus
@@ -6,17 +5,34 @@ from busMonitoring.models import ElectricBus
 class ElectricBusSerializer(serializers.ModelSerializer):
     class Meta:
         model = ElectricBus
-        fields = ['bus_id', 'battery_capacity', 'length']
+        fields = ['id', 'bus_id', 'battery']
 
 class LocationTypeSerializer(serializers.ModelSerializer):
     class Meta:
         model = LocationType
-        fields = [ "location_name", "description"]
+        fields = ["location_name", "description"]
+
 class DrivingScheduleSerializer(serializers.ModelSerializer):
-    departure_location = LocationTypeSerializer()
-    arrival_location = LocationTypeSerializer()
-    bus = ElectricBusSerializer()
+    # Input fields
+    departure_location = serializers.PrimaryKeyRelatedField(queryset=LocationType.objects.all())
+    arrival_location = serializers.PrimaryKeyRelatedField(queryset=LocationType.objects.all())
+    bus = serializers.PrimaryKeyRelatedField(queryset=ElectricBus.objects.all())
+
+    # Output fields
+    departure_location_name = serializers.SerializerMethodField()
+    arrival_location_name = serializers.SerializerMethodField()
+    bus_details = ElectricBusSerializer(source="bus", read_only=True)
+
+    def get_departure_location_name(self, obj):
+        return obj.departure_location.location_name if obj.departure_location else None
+
+    def get_arrival_location_name(self, obj):
+        return obj.arrival_location.location_name if obj.arrival_location else None
+
     class Meta:
         model = DrivingSchedule
-        fields = ['id', 'bus', 'departure_time', 'arrival_time','departure_location' , 'arrival_location']
-
+        fields = [
+            'id', 'bus', 'bus_details', 'departure_time', 'arrival_time',
+            'departure_location', 'departure_location_name',
+            'arrival_location', 'arrival_location_name'
+        ]
