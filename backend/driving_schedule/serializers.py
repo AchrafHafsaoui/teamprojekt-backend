04c086f3e22281d9edb3560afc297987486a13f1
Aziz Abouda <azizabouda0103@gmail.com>
2025-01-27 18:25:43 +0100

    driving schedule fixed again

--- backend/driving_schedule/serializers.py
+++ backend/driving_schedule/serializers.py
@@ -2,21 +2,27 @@ from rest_framework import serializers
 from .models import DrivingSchedule, LocationType
 from busMonitoring.models import ElectricBus
 
+
 class ElectricBusSerializer(serializers.ModelSerializer):
+    """Serializer for displaying detailed bus information."""
     class Meta:
         model = ElectricBus
         fields = ['id', 'bus_id', 'battery']
 
+
 class LocationTypeSerializer(serializers.ModelSerializer):
+    """Serializer for location types."""
     class Meta:
         model = LocationType
         fields = ["location_name", "description"]
 
+
 class DrivingScheduleSerializer(serializers.ModelSerializer):
-    # Output fields for location names
+    """Serializer for driving schedules."""
+    bus_details = ElectricBusSerializer(source='bus', read_only=True)  # Include nested bus details
+    bus = serializers.PrimaryKeyRelatedField(queryset=ElectricBus.objects.all(), required=False)
     departure_location_name = serializers.SerializerMethodField()
     arrival_location_name = serializers.SerializerMethodField()
-    bus = ElectricBusSerializer(read_only=True)  # Update to include bus details directly
 
     def get_departure_location_name(self, obj):
         return obj.departure_location.location_name if obj.departure_location else None
@@ -27,8 +33,8 @@ class DrivingScheduleSerializer(serializers.ModelSerializer):
     class Meta:
         model = DrivingSchedule
         fields = [
-            'id', 'bus', 'departure_time', 'arrival_time',
+            'id', 'bus', 'bus_details',
+            'departure_time', 'arrival_time',
             'departure_location', 'departure_location_name',
-            'arrival_location', 'arrival_location_name'
+            'arrival_location', 'arrival_location_name',
         ]
-
