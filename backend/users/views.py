cf5631f9b87c9958474da4b3de62c310ac6c8847
makkiboukhris <makki01boukhris@gmail.com>
2025-02-04 11:51:32 +0100

    code error 1

--- backend/users/views.py
+++ backend/users/views.py
@@ -1,3 +1,4 @@
+from django.http import JsonResponse
 import jwt
 from token_manage.tokens_utils import validate_access_token
 from rest_framework.views import APIView
@@ -49,17 +50,30 @@ class LoginView(APIView):
             )
             if user:
                 refresh = RefreshToken.for_user(user)
-                return Response({
-                        "access": str(refresh.access_token),
-                        "refresh": str(refresh),
+                access_token = str(refresh.access_token)
+
+                response = JsonResponse({
+                    "access": access_token,  
                 }, status=status.HTTP_200_OK)
+
+                response.set_cookie(
+                    key="refreshToken",  
+                    value=str(refresh),  
+                    httponly=True,       
+                    secure=True,         
+                    samesite='Strict',   
+                    max_age=6 * 60 * 60,  
+                )
+                return response
             return Response({"error": "Invalid credentials"}, status=401)
         return Response({"error": "creds missing"}, status=401)
                 
 
 class RefreshAccessTokenView(APIView):
     def post(self, request):
-        refresh_token = request.data.get('refresh')
+        refresh_token = request.COOKIES.get('refreshToken')
+        
+        print(f"reftkn: {refresh_token}")
         if not refresh_token:
             return Response({"error": "Refresh token is required"}, status=status.HTTP_400_BAD_REQUEST)
 
@@ -79,9 +93,7 @@ class RefreshAccessTokenView(APIView):
 class IsAuthView(APIView):
     def post(self, request):
         access_token = request.data.get('access')
-        print(f"Access Token: {access_token}")
         min_required_role = request.data.get('role')
-        print(f"min_required_role: {min_required_role}")
         if not access_token or not min_required_role:
             return Response(
                 {"detail": "Access token and role are required"},
