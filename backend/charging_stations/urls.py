a50f7e7a3359e142a0a2e552bc921fa285bf19ee
Youssef Graja <youssefgraja1965@gmail.com>
2025-02-09 18:55:09 +0100

    add/delete for charging stations

--- backend/charging_stations/urls.py
+++ backend/charging_stations/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import StationList
+from .views import StationList , add_charging_station
 
 router = DefaultRouter()
-router.register(r'stations',StationList)
+router.register(r'stations',StationList,basename="stations")
 
 urlpatterns = [
     path('api/', include(router.urls)),
+    path('api/charging-stations/', add_charging_station, name='add_charging_station'),
 ]
--- backend/charging_stations/views.py
+++ backend/charging_stations/views.py
@@ -2,6 +2,9 @@ from rest_framework.viewsets import ModelViewSet
 from rest_framework.filters import SearchFilter, OrderingFilter
 from .models import Station
 from .serializers import StationSerializer
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework import status
 
 class StationList(ModelViewSet):
     queryset = Station.objects.all()
@@ -9,3 +12,14 @@ class StationList(ModelViewSet):
     filter_backends = [SearchFilter, OrderingFilter]
     search_fields = ['station_id', 'availability']  # Enable search by these fields
     ordering_fields = ['charging_power']  # Enable ordering by these fields
+
+
+@api_view(['POST'])
+def add_charging_station(request):
+    serializer = StationSerializer(data=request.data)
+    if serializer.is_valid():
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
+    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
